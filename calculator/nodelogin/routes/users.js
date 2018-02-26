44cb30147269484b347681b01418089dc66d4a4d
KiratiBhuva <35611972+KiratiBhuva@users.noreply.github.com>
2018-02-26 01:52:49 -0800

    removing node modules

--- calculator/nodelogin/routes/users.js
+++ calculator/nodelogin/routes/users.js
@@ -2,30 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 
-var users = [
-    {
-        username: "Mike",
-        password: "mike123"
-    },
-    {
-        username: "Tom",
-        password: "tom123"
-    },
-    {
-        username: "John",
-        password: "john123"
-    },
-    {
-        username: "Mac",
-        password: "mac123"
-    }
-];
-
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-    res.send('respond with a resource');
-});
-
 router.post('/doLogin', function (req, res, next) {
 
     var reqUsername = req.body.username;
@@ -45,12 +21,6 @@ router.post('/doLogin', function (req, res, next) {
         res.status(401).json({message: "Login failed"});
     }
 
-
-    // if(theUser.password === reqPassword){
-    //     res.status(201).json({message: "Login successful"});
-    // } else {
-    //     res.status(401).json({message: "Login failed"});
-    // }
 
 });
 
